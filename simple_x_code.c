5224627227aa3f807d361da2442153f91d37bef0
Devot2025 <ota.drmr3.816@gmail.com>
2025-09-16 15:36:39 +0900

    Update simple_x_code.c

--- simple_x_code.c
+++ simple_x_code.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include "amh_in_src.h"
 #include "lexser_main_amh.h"
-#include "parser_main_amh.h"
 int main(void) {
 	Amh_Lex_Token_List * tmp = start_read_amh_lex_main("test.amh");
 	amh_token_list_call(tmp);
 	delete_amh_token_list(&tmp);
 	return 0;
+
 }
